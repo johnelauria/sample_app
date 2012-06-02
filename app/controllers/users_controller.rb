a66d6387e47055515084fd7c74c72eed5307b1f0
John Maenard Elauria <johnelauria@yahoo.com>
2012-06-02 14:17:41 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
+  def create
+  	@user = User.new(params[:user])
+
+  	if @user.save
+      flash[:success] = "Welcome #{@user.name}! Thank you for signing up to this Sample Application!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+  end
+
   def new
+  	@user = User.new
+
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @user }
+  	end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+
+  	respond_to do |format|
+  		format.html
+  		format.json { render json: @user}
+  	end
   end
 end

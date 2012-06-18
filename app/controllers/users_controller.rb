52e47b805ecceb871901edaa9033b2249721323a
John Maenard Elauria <johnelauria@yahoo.com>
2012-06-18 21:20:06 +0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def create
   	@user = User.new(params[:user])
 
@@ -29,4 +37,47 @@ class UsersController < ApplicationController
   		format.json { render json: @user}
   	end
   end
-end
+
+    
+  def edit
+    
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Account has been successfully deleted"
+    redirect_to users_path
+  end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please Sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      if !current_user?(@user)
+        flash[:error] = "You sir are under arrest for the violation of something"
+        redirect_to root_path
+      end
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+  end 
